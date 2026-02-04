6e1f876ead7ec0414829d93d58197bccd8971bc3
Daniel <onyemordidaniel@gmail.com>
2026-02-04 17:39:36 +0100

    all

--- src/app/reuseables/http-loader/request-data.service.ts
+++ src/app/reuseables/http-loader/request-data.service.ts
@@ -5,11 +5,7 @@ import { finalize, Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class RequestDataService {
   // private baseUrl = 'http://127.0.0.1:8000/api'; // Replace with your API endpoint
-<<<<<<< HEAD
-  private baseUrl = 'https://smgfb-fbd6d4fc5fb9.herokuapp.com/api'; // Replace with your API endpoint
-=======
   private baseUrl = 'https://smgfb-71e344e74163.herokuapp.com/api'; // Replace with your API endpoint
->>>>>>> 80e6739 (all)
 // dfg
   constructor(
     private http: HttpClient,
